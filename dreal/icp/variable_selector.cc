550314044feb2b9a4a1cce7b95fef5e623263d11
Soonho Kong <soonho.kong@tri.global>
2017-09-06 15:39:24 -0400

    refactor(icp): Remove variable_selector

--- dreal/icp/variable_selector.cc
+++
@@ -1,3 +0,0 @@
-#include "dreal/icp/variable_selector.h"
-
-namespace dreal {}
--- dreal/icp/variable_selector.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace dreal {
-
-// TODO(soonho): Implement VariableSelector.
-class VariableSelector {};
-
-}  // namespace dreal
