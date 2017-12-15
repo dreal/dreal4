005eac0aafd11afb765c23d4f64fe83f516a8c35
Soonho Kong <soonho.kong@tri.global>
2017-12-15 11:33:27 -0500

    script: Remove generate_compile_commands.sh
    Instead, we use the solution from https://github.com/grailbio/bazel-compilation-database.

--- scripts/generate_compile_commands.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-bazel build --experimental_action_listener=//tools/actions:generate_compile_commands_listener //...
-python3 tools/actions/generate_compile_commands_json.py
