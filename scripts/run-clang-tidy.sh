d3cce6ddc9b747b568f4552ed6767d97e8fef76a
Soonho Kong <soonho.kong@tri.global>
2018-01-22 12:00:50 -0500

    scripts: Update run-clang-tidy

--- scripts/run-clang-tidy.sh
+++ scripts/run-clang-tidy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 LAST_ARG=${@:$#}
+EXEC_ROOT=`bazel info execution_root`
+BAZEL_EXTERNAL=${EXEC_ROOT}/external
 
 clang-tidy $@ -header-filter=$(realpath .) -system-headers=0 -p ./ \
            -- \
@@ -9,12 +11,12 @@ clang-tidy $@ -header-filter=$(realpath .) -system-headers=0 -p ./ \
            -x c++ \
            -I bazel-genfiles \
 	   `pkg-config ibex --cflags` \
-           -isystem bazel-dreal4/external/spdlog/include \
-           -isystem bazel-dreal4/external/fmt \
-           -isystem bazel-dreal4/external/drake_symbolic \
-           -isystem bazel-dreal4/external/ezoptionparser \
-           -isystem bazel-dreal4/external/gtest/googletest/include \
-           -isystem bazel-dreal4/external/picosat \
+           -isystem ${BAZEL_EXTERNAL}/spdlog/include \
+           -isystem ${BAZEL_EXTERNAL}/fmt \
+           -isystem ${BAZEL_EXTERNAL}/drake_symbolic \
+           -isystem ${BAZEL_EXTERNAL}/ezoptionparser \
+           -isystem ${BAZEL_EXTERNAL}/gtest/googletest/include \
+           -isystem ${BAZEL_EXTERNAL}/picosat \
            -isystem /usr/local/opt/llvm/include/c++/v1 \
            -isystem /usr/local/include \
            -isystem /usr/local/opt/flex/include \
