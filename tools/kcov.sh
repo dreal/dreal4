6922814c166dcd4e08feca8c232b56d66fed2fa4
Soonho Kong <soonho.kong@tri.global>
2018-02-15 14:04:59 -0500

    bazel: Update bazel.rc and kcov script

--- tools/kcov.sh
+++ tools/kcov.sh
@@ -4,6 +4,12 @@
 me=$(python -c 'import os; print(os.path.realpath("'"$0"'"))')
 WORKSPACE=$(dirname $(dirname "$me"))
 
+# There must be ${WORKSPACE}/WORKSPACE.
+if [ ! -f "${WORKSPACE}/WORKSPACE" ]; then
+  echo "File not found: ${WORKSPACE}/WORKSPACE"
+  exit 1
+fi
+
 # Just in case, don't collect data from cpplint tests.
 if echo "$@" | grep -q _cpplint ; then
     "$@"
@@ -12,7 +18,7 @@ fi
 
 kcov \
     --include-path=$WORKSPACE \
-    --exclude-pattern=thirdParty,externals \
     $WORKSPACE/bazel-kcov \
     --replace-src-path=/proc/self/cwd:$WORKSPACE \
+    --exclude-pattern=third_party,test \
     "$@"
