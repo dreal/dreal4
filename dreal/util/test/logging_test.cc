e4db0a5264c6a28af31ab25f76f38ec6123a89ea
Soonho Kong <soonho.kong@tri.global>
2018-02-16 12:14:58 -0500

    test(util): Add logging_test

---
+++ dreal/util/test/logging_test.cc
@@ -0,0 +1,90 @@
+#include "dreal/util/logging.h"
+
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+namespace dreal {
+
+using std::cerr;
+using std::endl;
+
+GTEST_TEST(Logging, DREAL_LOG_TRACE_SIDE_EFFECT) {
+  int x = 0;
+
+  log()->set_level(spdlog::level::off);
+  // The following should cause no side-effects.
+  DREAL_LOG_TRACE("x = {}", x++);
+  EXPECT_EQ(x, 0);
+
+  log()->set_level(spdlog::level::critical);
+  // The following should cause no side-effects.
+  DREAL_LOG_TRACE("x = {}", x++);
+  EXPECT_EQ(x, 0);
+
+  log()->set_level(spdlog::level::err);
+  // The following should cause no side-effects.
+  DREAL_LOG_TRACE("x = {}", x++);
+  EXPECT_EQ(x, 0);
+
+  log()->set_level(spdlog::level::warn);
+  // The following should cause no side-effects.
+  DREAL_LOG_TRACE("x = {}", x++);
+  EXPECT_EQ(x, 0);
+
+  log()->set_level(spdlog::level::info);
+  // The following should cause no side-effects.
+  DREAL_LOG_TRACE("x = {}", x++);
+  EXPECT_EQ(x, 0);
+
+  log()->set_level(spdlog::level::debug);
+  // The following should cause no side-effects.
+  DREAL_LOG_TRACE("x = {}", x++);
+  EXPECT_EQ(x, 0);
+
+  log()->set_level(spdlog::level::trace);
+  // The following should cause side-effects.
+  DREAL_LOG_TRACE("x = {}", x++);
+  EXPECT_EQ(x, 1);
+}
+
+GTEST_TEST(Logging, DREAL_LOG_DEBUG_SIDE_EFFECT) {
+  int x = 0;
+
+  log()->set_level(spdlog::level::off);
+  // The following should cause no side-effects.
+  DREAL_LOG_DEBUG("x = {}", x++);
+  EXPECT_EQ(x, 0);
+
+  log()->set_level(spdlog::level::critical);
+  // The following should cause no side-effects.
+  DREAL_LOG_DEBUG("x = {}", x++);
+  EXPECT_EQ(x, 0);
+
+  log()->set_level(spdlog::level::err);
+  // The following should cause no side-effects.
+  DREAL_LOG_DEBUG("x = {}", x++);
+  EXPECT_EQ(x, 0);
+
+  log()->set_level(spdlog::level::warn);
+  // The following should cause no side-effects.
+  DREAL_LOG_DEBUG("x = {}", x++);
+  EXPECT_EQ(x, 0);
+
+  log()->set_level(spdlog::level::info);
+  // The following should cause no side-effects.
+  DREAL_LOG_DEBUG("x = {}", x++);
+  EXPECT_EQ(x, 0);
+
+  log()->set_level(spdlog::level::debug);
+  // The following should no side-effects.
+  DREAL_LOG_DEBUG("x = {}", x++);
+  EXPECT_EQ(x, 1);
+
+  log()->set_level(spdlog::level::trace);
+  // The following should cause side-effects.
+  DREAL_LOG_DEBUG("x = {}", x++);
+  EXPECT_EQ(x, 2);
+}
+
+}  // namespace dreal
