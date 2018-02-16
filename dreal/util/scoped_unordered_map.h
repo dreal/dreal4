062c002ef6571e4441b994c8586dbc2ea0cbc0f4
Soonho Kong <soonho.kong@tri.global>
2018-02-16 16:50:38 -0500

    feat(util): Add ScopedUnorderedMap

---
+++ dreal/util/scoped_unordered_map.h
@@ -0,0 +1,125 @@
+#pragma once
+
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <tuple>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "dreal/util/exception.h"
+
+namespace dreal {
+
+/// Backtrackable map.
+template <class Key, class T, class Hash = std::hash<Key>,
+          class KeyEqual = std::equal_to<Key>,
+          class Allocator = std::allocator<std::pair<const Key, T>>>
+class ScopedUnorderedMap {
+ public:
+  // Aliases.
+  using UnorderedMapType =
+      std::unordered_map<Key, T, Hash, KeyEqual, Allocator>;
+  using value_type = typename UnorderedMapType::value_type;
+  using size_type = typename UnorderedMapType::size_type;
+  using const_iterator = typename UnorderedMapType::const_iterator;
+
+  // To backtrack, we need to record the actions applied to this
+  // container.
+  enum class ActionKind {
+    INSERT,  ///< Insert(k, v) means that (k, v) is inserted.
+    UPDATE,  ///< Update(k, v) means that (k, v) was replaced by a new value.
+  };
+  using Action = std::tuple<ActionKind, Key, T>;
+
+  ScopedUnorderedMap() = default;
+  ScopedUnorderedMap(const ScopedUnorderedMap&) = default;
+  ScopedUnorderedMap(ScopedUnorderedMap&&) = default;
+  ScopedUnorderedMap& operator=(const ScopedUnorderedMap&) = default;
+  ScopedUnorderedMap& operator=(ScopedUnorderedMap&&) = default;
+  ~ScopedUnorderedMap() = default;
+
+  // Iterators.
+  //
+  // @note We only provide 'const' iterators because any modification
+  // should be done explicitly via its APIs so that we can keep track
+  // of changes and undo when pop() is called.
+  const_iterator begin() const { return map_.begin(); }
+  const_iterator cbegin() const { return map_.cbegin(); }
+  const_iterator end() const { return map_.end(); }
+  const_iterator cend() const { return map_.cend(); }
+
+  // Capacity
+  bool empty() const { return map_.empty(); }
+  size_type size() const { return map_.size(); }
+
+  // Modifiers
+  void clear() {
+    map_.clear();
+    actions_.clear();
+    stack_.clear();
+  }
+  void insert(const Key& k, const T& v) {
+    auto it = map_.find(k);
+    if (it == map_.end()) {
+      // Case 1) k does not exist.
+      actions_.emplace_back(ActionKind::INSERT, k, v);
+      map_.emplace(k, v);
+    } else {
+      // Case 2) k exists. Save the old value so that we can roll
+      // back later.
+      actions_.emplace_back(ActionKind::UPDATE, k, it->second);
+      it->second = v;  // Perform Update.
+    }
+  }
+
+  // Lookup
+  const T& operator[](const Key& key) const {
+    const auto it = map_.find(key);
+    if (it == map_.end()) {
+      throw DREAL_RUNTIME_ERROR(
+          "ScopedUnorderedMap has no entry for the key {}.", key);
+    } else {
+      return it->second;
+    }
+  }
+  size_type count(const Key& key) const { return map_.count(key); }
+  // @note It returns 'const' iterator.
+  const_iterator find(const Key& key) const { return map_.find(key); }
+
+  // Push/Pop
+  void push() { stack_.push_back(actions_.size()); }
+  void pop() {
+    if (stack_.empty()) {
+      throw DREAL_RUNTIME_ERROR(
+          "ScopedUnorderedMap cannot be popped because it's scope is empty.");
+    }
+    size_type idx = stack_.back();
+    while (idx < actions_.size()) {
+      const Action& item{actions_.back()};
+      const ActionKind kind{std::get<0>(item)};
+      const Key& k{std::get<1>(item)};
+      const T& v{std::get<2>(item)};
+      auto it = map_.find(k);
+      switch (kind) {
+        case ActionKind::INSERT:
+          // Remove (k, v).
+          map_.erase(it);
+          break;
+        case ActionKind::UPDATE:
+          // Roll back to map_[k] = v.
+          it->second = v;
+          break;
+      }
+      actions_.pop_back();
+    }
+    stack_.pop_back();
+  }
+
+ private:
+  std::vector<Action> actions_;
+  std::vector<size_type> stack_;
+  UnorderedMapType map_;
+};
+}  // namespace dreal
---
+++ dreal/util/test/scoped_unordered_map_test.cc
@@ -0,0 +1,121 @@
+#include "dreal/util/scoped_unordered_map.h"
+
+#include <gtest/gtest.h>
+
+namespace dreal {
+
+GTEST_TEST(ScopedUnorderedMapTest, CopyConstruct) {
+  ScopedUnorderedMap<int, int> map1;
+  map1.insert(1, 2);
+  map1.insert(2, 3);
+  map1.insert(3, 4);
+
+  const ScopedUnorderedMap<int, int> map2(map1);
+  EXPECT_EQ(map1.size(), map2.size());
+  for (const auto& p : map1) {
+    const int& k = p.first;
+    EXPECT_EQ(map1[k], map2[k]);
+  }
+}
+
+GTEST_TEST(ScopedUnorderedMapTest, MoveConstruct) {
+  ScopedUnorderedMap<int, int> map1;
+  map1.insert(1, 2);
+  map1.insert(2, 3);
+  map1.insert(3, 4);
+
+  const ScopedUnorderedMap<int, int> map2(std::move(map1));
+  EXPECT_EQ(map2.size(), 3);
+  EXPECT_EQ(map2[1], 2);
+  EXPECT_EQ(map2[2], 3);
+  EXPECT_EQ(map2[3], 4);
+}
+
+GTEST_TEST(ScopedUnorderedMapTest, CopyOp) {
+  ScopedUnorderedMap<int, int> map1;
+  map1.insert(1, 2);
+  map1.insert(2, 3);
+  map1.insert(3, 4);
+
+  ScopedUnorderedMap<int, int> map2;
+  map2 = map1;
+  EXPECT_EQ(map1.size(), map2.size());
+  for (const auto& p : map1) {
+    const int& k = p.first;
+    EXPECT_EQ(map1[k], map2[k]);
+  }
+}
+
+GTEST_TEST(ScopedUnorderedMapTest, MoveOp) {
+  ScopedUnorderedMap<int, int> map1;
+  map1.insert(1, 2);
+  map1.insert(2, 3);
+  map1.insert(3, 4);
+
+  ScopedUnorderedMap<int, int> map2;
+  map2 = std::move(map1);
+  EXPECT_EQ(map2.size(), 3);
+  EXPECT_EQ(map2[1], 2);
+  EXPECT_EQ(map2[2], 3);
+  EXPECT_EQ(map2[3], 4);
+}
+
+GTEST_TEST(ScopedUnorderedMapTest, Clear) {
+  ScopedUnorderedMap<int, int> map1;
+  map1.insert(1, 2);
+  map1.push();
+  map1.insert(2, 3);
+  map1.insert(3, 4);
+  EXPECT_EQ(map1.size(), 3);
+
+  map1.clear();
+  EXPECT_EQ(map1.size(), 0);
+
+  EXPECT_THROW(map1.pop(), std::runtime_error);
+}
+
+GTEST_TEST(ScopedUnorderedMapTest, Update) {
+  ScopedUnorderedMap<int, int> map;
+  map.insert(1, 2);
+  EXPECT_EQ(map[1], 2);
+  EXPECT_EQ(map.size(), 1);
+
+  map.insert(1, 3);  // Update
+  EXPECT_EQ(map[1], 3);
+  EXPECT_EQ(map.size(), 1);
+
+  EXPECT_THROW(map[2], std::runtime_error);
+}
+
+GTEST_TEST(ScopedUnorderedMapTest, PushPop) {
+  ScopedUnorderedMap<int, int> map;
+  EXPECT_TRUE(map.empty());
+
+  map.push();
+
+  map.insert(1, 2);
+  EXPECT_EQ(map[1], 2);
+  EXPECT_EQ(map.size(), 1);
+  EXPECT_FALSE(map.empty());
+
+  map.push();
+
+  map.insert(1, 3);  // Update
+  map.insert(2, 4);
+  EXPECT_EQ(map.size(), 2);
+  EXPECT_EQ(map[1], 3);
+  EXPECT_EQ(map[2], 4);
+  EXPECT_FALSE(map.empty());
+
+  map.pop();
+
+  EXPECT_EQ(map.size(), 1);
+  EXPECT_EQ(map[1], 2);        // Old value should be restored.
+  EXPECT_EQ(map.count(2), 0);  // the entry for '2' is deleted.
+  EXPECT_FALSE(map.empty());
+
+  map.pop();
+  EXPECT_TRUE(map.empty());
+}
+
+}  // namespace dreal
