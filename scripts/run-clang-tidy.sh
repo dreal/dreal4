5ea91fa57c07e60a394afa61c0622de6cb79928e
Soonho Kong <soonho.kong@tri.global>
2018-03-27 16:13:48 -0400

    chore: Update .clang-tidy

--- scripts/run-clang-tidy.sh
+++ scripts/run-clang-tidy.sh
@@ -10,7 +10,12 @@ clang-tidy $@ -header-filter=$(realpath .) -system-headers=0 -p ./ \
            -I./ \
            -x c++ \
            -I bazel-genfiles \
-	   `PKG_CONFIG_PATH=/usr/local/opt/ibex@2.6.5/share/pkgconfig/ pkg-config ibex --cflags` \
+	   -I/usr/local/opt/ibex@2.6.5/include \
+	   -I/usr/local/opt/ibex@2.6.5/include/ibex \
+	   -I/usr/local/opt/ibex@2.6.5/include/ibex/3rd \
+	   -I/usr/local/opt/clp/include/clp/coin \
+	   -I/usr/local/opt/coinutils/include/coinutils/coin \
+	   -I/usr/local/opt/python@2/Frameworks/Python.framework/Versions/2.7/include/python2.7 \
            -isystem ${BAZEL_EXTERNAL}/spdlog/include \
            -isystem ${BAZEL_EXTERNAL}/fmt \
            -isystem ${BAZEL_EXTERNAL}/drake_symbolic \
