504b9457680adcb176eb16315fa9006969fcee9a
Soonho Kong <soonho.kong@tri.global>
2018-04-13 06:17:04 -0400

    feat(api/api_py.cc): Add API functions using Config class

--- dreal/api/api_py.cc
+++ dreal/api/api_py.cc
@@ -27,35 +27,29 @@ PYBIND11_MODULE(_api_py, m) {
   m.def("CheckSatisfiability",
         py::overload_cast<const Formula&, double>(&CheckSatisfiability));
 
-  // TODO(soonho): Enable this.
-  // m.def("CheckSatisfiability",
-  //       py::overload_cast<const Formula&, Config>(&CheckSatisfiability));
+  m.def("CheckSatisfiability",
+        py::overload_cast<const Formula&, Config>(&CheckSatisfiability));
 
   m.def("CheckSatisfiability",
         py::overload_cast<const Formula&, double, Box*>(&CheckSatisfiability));
 
-  // TODO(soonho): Enable this.
-  // m.def("CheckSatisfiability",
-  //       py::overload_cast<const Formula&, Config,
-  //       Box*>(&CheckSatisfiability));
+  m.def("CheckSatisfiability",
+        py::overload_cast<const Formula&, Config, Box*>(&CheckSatisfiability));
 
   m.def(
       "Minimize",
       py::overload_cast<const Expression&, const Formula&, double>(&Minimize));
 
-  // TODO(soonho): Enable this.
-  // m.def(
-  //     "Minimize",
-  //     py::overload_cast<const Expression&, const Formula&,
-  //     Config>(&Minimize));
+  m.def(
+      "Minimize",
+      py::overload_cast<const Expression&, const Formula&, Config>(&Minimize));
 
   m.def("Minimize",
         py::overload_cast<const Expression&, const Formula&, double, Box*>(
             &Minimize));
 
-  // TODO(soonho): Enable this.
-  // m.def("Minimize",
-  //       py::overload_cast<const Expression&, const Formula&, Config, Box*>(
-  //           &Minimize));
+  m.def("Minimize",
+        py::overload_cast<const Expression&, const Formula&, Config, Box*>(
+            &Minimize));
 }
 }  // namespace dreal
