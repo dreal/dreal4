d3d15429ee5668fbbbe5f7ee20659770bdd146b7
Soonho Kong <soonho.kong@tri.global>
2018-05-02 02:40:06 -0400

    chore: Version 4.18.05.1

--- setup/ubuntu/16.04/install.sh
+++ setup/ubuntu/16.04/install.sh
@@ -12,10 +12,10 @@ add-apt-repository ppa:dreal/dreal -y  # For libibex-dev
 apt update
 
 # Install dReal
-DREAL_VERSION=4.18.04.1
+DREAL_VERSION=4.18.05.1
 DREAL_DEBNAME=dreal_${DREAL_VERSION}_amd64.deb
 DREAL_URL=https://dl.bintray.com/dreal/dreal/${DREAL_DEBNAME}
-DREAL_SHA256=036a36bf95d540af4f23688c04950b90c691065db8326d9f1888655feac5a985
+DREAL_SHA256=7a6ab55bfd94856c3d1742753b0486d7d7176ad59c93160a2f011e3c93bd991b
 apt install --no-install-recommends wget -y
 wget ${DREAL_URL}
 if echo "${DREAL_SHA256}  ${DREAL_DEBNAME}" | sha256sum -c; then
--- tools/dreal.bzl
+++ tools/dreal.bzl
@@ -1,7 +1,7 @@
 # Based on Drake's drake.bzl file,
 # https://github.com/RobotLocomotion/drake/blob/master/tools/drake.bzl.
 
-DREAL_VERSION = "4.18.04.1"
+DREAL_VERSION = "4.18.05.1"
 
 DREAL_PREFIX = "opt/dreal/%s" % DREAL_VERSION
 
