a8e4ee96b63c4402db1605aa0005bb3386881dfb
Soonho Kong <soonho.kong@tri.global>
2018-11-13 14:43:06 -0500

    scripts: Add update_dreal_version.sh

---
+++ scripts/update_dreal_version.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+OLD_VERSION=$1
+NEW_VERSION=$2
+sed -i "s/${OLD_VERSION}/${NEW_VERSION}/g" `find . -type f`
+
+DREAL_DEBNAME=dreal_${NEW_VERSION}_amd64.deb
+DREAL_URL=https://dl.bintray.com/dreal/dreal/${DREAL_DEBNAME}
+
+wget ${DREAL_URL} -O ${DREAL_DEBNAME}
+SHA256=`shasum -a 256 ${DREAL_DEBNAME} | cut -d ' ' -f 1`
+rm ${DREAL_DEBNAME}
+sed -i "s/DREAL_SHA256=.*/DREAL_SHA256=${SHA256}/g" `find setup -type f`
