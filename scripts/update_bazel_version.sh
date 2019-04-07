08ea63d58b659eaa680e7a7a6065e2a70fdf2455
Soonho Kong <soonho.kong@tri.global>
2019-04-06 23:52:58 -0400

    scripts(update_bazel_version.sh): Add usage and root_path

--- scripts/update_bazel_version.sh
+++ scripts/update_bazel_version.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+display_usage() {
+    echo "usage: $0 <new_version_string>"
+}
+
+if [ $# -ne 1 ]
+then
+    display_usage
+    exit 1
+fi
+
+SCRIPT_PATH="`dirname \"$0\"`"
+ROOT_PATH="${SCRIPT_PATH}/.."
 VERSION=$1
 DEB=bazel_${VERSION}-linux-x86_64.deb
 URL=https://github.com/bazelbuild/bazel/releases/download/${VERSION}/${DEB}
@@ -6,5 +20,5 @@ wget ${URL} -O ${DEB}
 SHA256=`shasum -a 256 ${DEB} | cut -d ' ' -f 1`
 rm ${DEB}
 
-sed -i "s/BAZEL_VERSION=.*/BAZEL_VERSION=${VERSION}/g" `find setup -type f`
-sed -i "s/BAZEL_SHA256=.*/BAZEL_SHA256=${SHA256}/g" `find setup -type f`
+sed -i "s/BAZEL_VERSION=.*/BAZEL_VERSION=${VERSION}/g" `find ${ROOT_PATH}/setup -type f`
+sed -i "s/BAZEL_SHA256=.*/BAZEL_SHA256=${SHA256}/g" `find ${ROOT_PATH}/setup -type f`
