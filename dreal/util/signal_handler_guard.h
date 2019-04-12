cb123ed07fd11a61e73aad2f4790ec66dc3d0363
Soonho Kong <soonho.kong@tri.global>
2019-04-12 06:14:18 -0400

    feat(util): Add SignalHandlerGuard

---
+++ dreal/util/signal_handler_guard.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <signal.h>
+#include <atomic>
+
+namespace dreal {
+
+/// Sets a new signal handler and restores the old one when it goes
+/// out of scope. When the flag is set, its destructor clears it out.
+///
+/// Motivation
+/// ----------
+///
+/// Python's signal handler merely set a flag which is only checked in
+/// the next python instruction. As a result, "C/C++ code may run
+/// uninterrupted for an arbitrary amount of time" (from :
+/// https://docs.python.org/3/library/signal.html#execution-of-python-signal-handlers)
+///
+/// Our approach
+/// ------------
+///
+/// We provide a custom signal handler for SIGINT, which sets a global
+/// flag (g_interrupted) which is monitored inside of dReal code. When
+/// the flag is set, the checking code is supposed to throw an
+/// exception, which will terminate the C++ call gracefully.  The C++
+/// exception will be handled by pybind11, which will translate it to
+/// a Python exception.
+class SignalHandlerGuard {
+ public:
+  using handler_t = void (*)(int);
+
+  /// Registers the new handler and saves the current one.
+  SignalHandlerGuard(int sig, handler_t handler,
+                           volatile std::atomic_bool* flag);
+
+  /// Restores the old signal handler. If the flag is set, clear it.
+  ~SignalHandlerGuard();
+
+ private:
+  const int sig_{0};
+  volatile std::atomic_bool* flag_;
+  struct sigaction old_action_;
+};
+}  // namespace dreal
