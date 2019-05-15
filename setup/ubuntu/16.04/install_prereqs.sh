15d59c1dbe9fe4d412f92a807a3c669c6ab34fed
Soonho Kong <soonho.kong@tri.global>
2019-05-15 09:23:13 -0400

    bazel: Use 0.25.2

--- setup/ubuntu/16.04/install_prereqs.sh
+++ setup/ubuntu/16.04/install_prereqs.sh
@@ -24,10 +24,10 @@ EOF
 )
 
 # Install bazel
-BAZEL_VERSION=0.24.1
+BAZEL_VERSION=0.25.2
 BAZEL_DEBNAME=bazel_${BAZEL_VERSION}-linux-x86_64.deb
 BAZEL_URL=https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${BAZEL_DEBNAME}
-BAZEL_SHA256=b6fec7719046953f8b9b18bb798b8d11a84147b936c31e4cfe8d1e6184cc24ed
+BAZEL_SHA256=7ca3bbec5730afc3732514cbebb1e8f451df455d85d6bed5eb5614c2bfe36f13
 apt-get install -y --no-install-recommends wget
 wget ${BAZEL_URL}
 if echo "${BAZEL_SHA256}  ${BAZEL_DEBNAME}" | sha256sum -c; then
