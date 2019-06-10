75e61fc4b6445a8787fb8a9a5f0d8d86da5aeed3
Soonho Kong <soonho.kong@tri.global>
2019-06-10 15:42:31 -0400

    feat(contractor/contractor_forall): Make forall contractor thread-safe

--- dreal/contractor/contractor_forall.h
+++ dreal/contractor/contractor_forall.h
@@ -5,6 +5,9 @@
 #include <ostream>
 #include <stdexcept>
 #include <utility>
+#include <vector>
+
+#include "ThreadPool/ThreadPool.h"
 
 #include "dreal/contractor/contractor.h"
 #include "dreal/contractor/contractor_cell.h"
@@ -213,7 +216,9 @@ class ContractorForall : public ContractorCell {
     cs->AddUsedConstraint(f_);
   }
 
-  std::ostream& display(std::ostream& os) const override { return os << f_; }
+  std::ostream& display(std::ostream& os) const override {
+    return os << "ContractorForall(" << f_ << ")";
+  }
 
  private:
   static Box ExtendBox(Box box, const Variables& vars) {
@@ -236,10 +241,89 @@ class ContractorForall : public ContractorCell {
 };
 
 template <typename ContextType>
+class ContractorForallMt : public ContractorCell {
+ public:
+  /// Deleted default constructor.
+  ContractorForallMt() = delete;
+
+  /// Constructs ForallMt contractor using @p f and @p box.
+  ContractorForallMt(Formula f, const Box& box, double epsilon,
+                     double inner_delta, const Config& config)
+      : ContractorCell{Contractor::Kind::FORALL,
+                       ibex::BitSet::empty(box.size()), config},
+        f_{std::move(f)},
+        epsilon_{epsilon},
+        inner_delta_{inner_delta},
+        ctc_ready_(config.number_of_jobs(), 0),
+        ctcs_(ctc_ready_.size()) {
+    ContractorForall<ContextType>* const ctc{GetCtcOrCreate(box)};
+    DREAL_ASSERT(ctc);
+    // Build input.
+    mutable_input() = ctc->input();
+  }
+
+  /// Deleted copy constructor.
+  ContractorForallMt(const ContractorForallMt&) = delete;
+
+  /// Deleted move constructor.
+  ContractorForallMt(ContractorForallMt&&) = delete;
+
+  /// Deleted copy assign operator.
+  ContractorForallMt& operator=(const ContractorForallMt&) = delete;
+
+  /// Deleted move assign operator.
+  ContractorForallMt& operator=(ContractorForallMt&&) = delete;
+
+  ~ContractorForallMt() override = default;
+
+  void Prune(ContractorStatus* cs) const override {
+    ContractorForall<ContextType>* const ctc{GetCtcOrCreate(cs->box())};
+    DREAL_ASSERT(ctc);
+    return ctc->Prune(cs);
+    // TODO(soonho): propagate output.
+  }
+
+  std::ostream& display(std::ostream& os) const override {
+    return os << "ContractorForall(" << f_ << ")";
+  }
+
+ private:
+  ContractorForall<ContextType>* GetCtcOrCreate(const Box& box) const {
+    const int tid{ThreadPool::get_thread_id()};
+    DREAL_ASSERT(tid == ThreadPool::get_thread_id());
+    DREAL_ASSERT(0 <= tid && tid <= static_cast<int>(ctc_ready_.size()));
+    if (ctc_ready_[tid]) {
+      return ctcs_[tid].get();
+    }
+    Config inner_config{config()};
+    inner_config.mutable_number_of_jobs() = 1;  // FORCE SEQ ICP in INNER LOOP
+    auto ctc_unique_ptr = std::make_unique<ContractorForall<ContextType>>(
+        f_, box, epsilon_, inner_delta_, inner_config);
+    ContractorForall<ContextType>* ctc{ctc_unique_ptr.get()};
+    DREAL_ASSERT(ctc);
+    ctcs_[tid] = std::move(ctc_unique_ptr);
+    ctc_ready_[tid] = 1;
+    return ctc;
+  }
+
+  const Formula f_;
+  const double epsilon_{};
+  const double inner_delta_{};
+
+  mutable std::vector<int> ctc_ready_;
+  mutable std::vector<std::unique_ptr<ContractorForall<ContextType>>> ctcs_;
+};
+
+template <typename ContextType>
 Contractor make_contractor_forall(Formula f, const Box& box, double epsilon,
                                   double inner_delta, const Config& config) {
-  return Contractor{std::make_shared<ContractorForall<ContextType>>(
-      std::move(f), box, epsilon, inner_delta, config)};
+  if (config.number_of_jobs() > 1) {
+    return Contractor{std::make_shared<ContractorForallMt<ContextType>>(
+        std::move(f), box, epsilon, inner_delta, config)};
+  } else {
+    return Contractor{std::make_shared<ContractorForall<ContextType>>(
+        std::move(f), box, epsilon, inner_delta, config)};
+  }
 }
 
 /// Converts @p contractor to ContractorForall.
