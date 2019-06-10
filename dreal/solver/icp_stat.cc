fb8393473713fab2087e4b2941d19d5af6c8a16e
Soonho Kong <soonho.kong@tri.global>
2019-06-10 15:06:59 -0400

    feat(solver/icp_stat): Make thread safe

--- dreal/solver/icp_stat.cc
+++ dreal/solver/icp_stat.cc
@@ -12,21 +12,23 @@ namespace dreal {
 IcpStat::~IcpStat() {
   if (enabled()) {
     using fmt::print;
-    print(cout, "{:<45} @ {:<20} = {:>15}\n", "Total # of Branching",
-          "ICP level", num_branch_);
-    print(cout, "{:<45} @ {:<20} = {:>15}\n", "Total # of Pruning", "ICP level",
-          num_prune_);
+    print(cout, "{:<45} @ {:<16} T{:<2} = {:>15}\n", "Total # of Branching",
+          "ICP level", thread_id_, num_branch_);
+    print(cout, "{:<45} @ {:<16} T{:<2} = {:>15}\n", "Total # of Pruning",
+          "ICP level", thread_id_, num_prune_);
     if (num_branch_) {
-      print(cout, "{:<45} @ {:<20} = {:>15f} sec\n",
-            "Total time spent in Branching", "ICP level",
+      print(cout, "{:<45} @ {:<16} T{:<2} = {:>15f} sec\n",
+            "Total time spent in Branching", "ICP level", thread_id_,
             timer_branch_.seconds());
     }
     if (num_prune_) {
-      print(cout, "{:<45} @ {:<20} = {:>15f} sec\n",
-            "Total time spent in Pruning", "ICP level", timer_prune_.seconds());
+      print(cout, "{:<45} @ {:<16} T{:<2} = {:>15f} sec\n",
+            "Total time spent in Pruning", "ICP level", thread_id_,
+            timer_prune_.seconds());
     }
-    print(cout, "{:<45} @ {:<20} = {:>15f} sec\n",
-          "Total time spent in Evaluation", "ICP level", timer_eval_.seconds());
+    print(cout, "{:<45} @ {:<16} T{:<2} = {:>15f} sec\n",
+          "Total time spent in Evaluation", "ICP level", thread_id_,
+          timer_eval_.seconds());
   }
 }
 }  // namespace dreal
--- dreal/solver/icp_stat.h
+++ dreal/solver/icp_stat.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <atomic>
+
 #include "dreal/util/stat.h"
 #include "dreal/util/timer.h"
 
@@ -10,15 +12,19 @@ namespace dreal {
 /// branching and pruning operations.
 class IcpStat : public Stat {
  public:
-  explicit IcpStat(const bool enabled) : Stat{enabled} {}
+  explicit IcpStat(const bool enabled, int id = 0)
+      : Stat{enabled}, thread_id_{id} {}
   IcpStat(const IcpStat&) = delete;
   IcpStat(IcpStat&&) = delete;
   IcpStat& operator=(const IcpStat&) = delete;
   IcpStat& operator=(IcpStat&&) = delete;
   ~IcpStat() override;
 
-  int num_branch_{0};
-  int num_prune_{0};
+  const int thread_id_;
+
+  std::atomic<int> num_branch_{0};
+  std::atomic<int> num_prune_{0};
+
   Timer timer_branch_;
   Timer timer_prune_;
   Timer timer_eval_;
