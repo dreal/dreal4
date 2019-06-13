soonho-tri/dreal3-apache2

feat(solver/config): Add number_of_jobs field

dreal/solver/config.cc
@@ -44,6 +44,9 @@ OptionValue<bool>& Config::mutable_use_parallel_icp() {
   return use_parallel_icp_;
 }
 
+int Config::number_of_jobs() const { return number_of_jobs_.get(); }
+OptionValue<int>& Config::mutable_number_of_jobs() { return number_of_jobs_; }
+
 bool Config::stack_left_box_first() const {
   return stack_left_box_first_.get();
 }
@@ -108,6 +111,7 @@ ostream& operator<<(ostream& os, const Config& config) {
              "use_worklist_fixpoint = {}, "
              "use_local_optimization = {}, "
              "use_parallel_icp = {}, "
+             "number_of_jobs = {}, "
              "nlopt_ftol_rel = {}, "
              "nlopt_ftol_abs = {}, "
              "nlopt_maxeval = {}, "
@@ -118,9 +122,10 @@ ostream& operator<<(ostream& os, const Config& config) {
              config.precision(), config.produce_models(), config.use_polytope(),
              config.use_polytope_in_forall(), config.use_worklist_fixpoint(),
              config.use_local_optimization(), config.use_parallel_icp(),
-             config.nlopt_ftol_rel(), config.nlopt_ftol_abs(),
-             config.nlopt_maxeval(), config.nlopt_maxtime(),
-             config.sat_default_phase(), config.random_seed());
+             config.number_of_jobs(), config.nlopt_ftol_rel(),
+             config.nlopt_ftol_abs(), config.nlopt_maxeval(),
+             config.nlopt_maxtime(), config.sat_default_phase(),
+             config.random_seed());
 }
 
 }  // namespace dreal

dreal/dreal4

dreal/solver/config.cc
@@ -39,6 +39,9 @@ OptionValue<bool>& Config::mutable_use_local_optimization() {
   return use_local_optimization_;
 }
 
+int Config::number_of_jobs() const { return number_of_jobs_.get(); }
+OptionValue<int>& Config::mutable_number_of_jobs() { return number_of_jobs_; }
+
 bool Config::stack_left_box_first() const {
   return stack_left_box_first_.get();
 }
@@ -102,6 +105,7 @@ ostream& operator<<(ostream& os, const Config& config) {
              "use_polytope_in_forall = {}, "
              "use_worklist_fixpoint = {}, "
              "use_local_optimization = {}, "
+             "number_of_jobs = {}, "
              "nlopt_ftol_rel = {}, "
              "nlopt_ftol_abs = {}, "
              "nlopt_maxeval = {}, "
@@ -111,10 +115,10 @@ ostream& operator<<(ostream& os, const Config& config) {
              ")",
              config.precision(), config.produce_models(), config.use_polytope(),
              config.use_polytope_in_forall(), config.use_worklist_fixpoint(),
-             config.use_local_optimization(), config.nlopt_ftol_rel(),
-             config.nlopt_ftol_abs(), config.nlopt_maxeval(),
-             config.nlopt_maxtime(), config.sat_default_phase(),
-             config.random_seed());
+             config.use_local_optimization(), config.number_of_jobs(),
+             config.nlopt_ftol_rel(), config.nlopt_ftol_abs(),
+             config.nlopt_maxeval(), config.nlopt_maxtime(),
+             config.sat_default_phase(), config.random_seed());
 }
 
 }  // namespace dreal