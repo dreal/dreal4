2a2f7f814bc39105bf4a6d78327a1f93be0e8bb7
Soonho Kong <soonho.kong@tri.global>
2019-06-17 14:27:32 -0400

    version: 4.19.06.3

--- setup/ubuntu/18.04/install.sh
+++ setup/ubuntu/18.04/install.sh
@@ -12,10 +12,10 @@ add-apt-repository ppa:dreal/dreal -y  # For libibex-dev
 apt-get update
 
 # Install dReal
-DREAL_VERSION=4.19.06.2
+DREAL_VERSION=4.19.06.3
 DREAL_DEBNAME=dreal_${DREAL_VERSION}_amd64.deb
 DREAL_URL=https://dl.bintray.com/dreal/dreal/${DREAL_DEBNAME}
-DREAL_SHA256=512a9aeabc883b2877252ed4ae199939e234ff9a1c776fc10baad02a8d357f82
+DREAL_SHA256=a8c7efc07de3b459eb63e9b54a475847bd5c87443e10ce87193185eb52017f6a
 apt-get install --no-install-recommends wget -y
 wget ${DREAL_URL}
 if echo "${DREAL_SHA256}  ${DREAL_DEBNAME}" | sha256sum -c; then
