0951b44bec88669d3d5c8a1ee77fc350c064f845
Soonho Kong <soonho.kong@tri.global>
2019-12-20 15:58:42 -0500

    bazel: Use bazel-2.0.0

--- setup/ubuntu/18.04/install_prereqs.sh
+++ setup/ubuntu/18.04/install_prereqs.sh
@@ -24,10 +24,10 @@ EOF
 )
       
 # Install bazel
-BAZEL_VERSION=1.2.1
+BAZEL_VERSION=2.0.0
 BAZEL_DEBNAME=bazel_${BAZEL_VERSION}-linux-x86_64.deb
 BAZEL_URL=https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${BAZEL_DEBNAME}
-BAZEL_SHA256=4bbb2718d451db5922223fda3aed3810c4ca50f431481e86a7fec4c585f18b1f
+BAZEL_SHA256=575c77b96bc6202f83c7ff233cd44541aa30ff1b2bc211eaf8c99a8987272c68
 apt-get install -y --no-install-recommends wget
 wget ${BAZEL_URL}
 if echo "${BAZEL_SHA256}  ${BAZEL_DEBNAME}" | sha256sum -c; then
