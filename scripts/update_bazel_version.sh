0dcf1f15f33e75513034139d8b5427625bc265ad
Soonho Kong <soonho.kong@tri.global>
2020-03-10 14:28:50 -0400

    fix(script): update_bazel_version.sh

--- scripts/update_bazel_version.sh
+++ scripts/update_bazel_version.sh
@@ -11,8 +11,8 @@ then
     exit 1
 fi
 
-SCRIPT_PATH="$(dirname \""$0"\")"
-ROOT_PATH="${SCRIPT_PATH}/.."
+SCRIPT_PATH="$(dirname "$0")"
+ROOT_PATH="${SCRIPT_PATH}"/..
 VERSION=$1
 DEB=bazel_${VERSION}-linux-x86_64.deb
 URL=https://github.com/bazelbuild/bazel/releases/download/${VERSION}/${DEB}
@@ -20,5 +20,5 @@ wget "${URL}" -O "${DEB}"
 SHA256=$(shasum -a 256 "${DEB}" | cut -d ' ' -f 1)
 rm "${DEB}"
 
-sed -i "s/BAZEL_VERSION=.*/BAZEL_VERSION=${VERSION}/g" "$(find "${ROOT_PATH}"/setup -type f)"
-sed -i "s/BAZEL_SHA256=.*/BAZEL_SHA256=${SHA256}/g" "$(find "${ROOT_PATH}"/setup -type f)"
+sed -i "s/BAZEL_VERSION=.*/BAZEL_VERSION=${VERSION}/g" $(find "${ROOT_PATH}"/setup -type f)
+sed -i "s/BAZEL_SHA256=.*/BAZEL_SHA256=${SHA256}/g" $(find "${ROOT_PATH}"/setup -type f)
