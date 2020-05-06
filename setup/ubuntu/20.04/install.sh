8f479c7a42010e5426337f1a3d201d7cd116ee92
Soonho Kong <soonho.kong@tri.global>
2020-05-05 22:55:22 -0700

    setup: Add support Ubuntu-20.04

---
+++ setup/ubuntu/20.04/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euo pipefail
+
+if [[ "${EUID}" -ne 0 ]]; then
+  echo 'This script must be run as root' >&2
+  exit 1
+fi
+
+apt-get update;
+apt-get install -y software-properties-common
+add-apt-repository ppa:dreal/dreal -y  # For libibex-dev
+apt-get update
+
+# Install dReal
+DREAL_VERSION=4.20.04.1
+DREAL_DEBNAME=dreal_${DREAL_VERSION}_amd64.deb
+DREAL_URL=https://dl.bintray.com/dreal/dreal/${DREAL_DEBNAME}
+DREAL_SHA256=75e768d2bb7fef9225ea7ba4d319340ef3daef7793dadee4bfa1f67fa1bd6dbf
+apt-get install --no-install-recommends wget -y
+wget "${DREAL_URL}"
+if echo "${DREAL_SHA256}  ${DREAL_DEBNAME}" | sha256sum -c; then
+    apt-get install -y ./"${DREAL_DEBNAME}"
+    rm "${DREAL_DEBNAME}"
+else
+    echo "SHA256 does not match ${DREAL_DEBNAME}:"
+    echo "    expected: ${DREAL_SHA256}"
+    echo "    actual  : $(sha256sum "${DREAL_DEBNAME}")"
+    exit 1
+fi
---
+++ setup/ubuntu/20.04/install_prereqs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -euo pipefail
+
+if [[ "${EUID}" -ne 0 ]]; then
+  echo 'This script must be run as root' >&2
+  exit 1
+fi
+apt-get update
+apt-get install -y --no-install-recommends software-properties-common
+add-apt-repository ppa:dreal/dreal -y  # For libibex-dev
+apt-get update
+apt-get install -y --no-install-recommends $(tr '\n' ' ' <<EOF
+bison
+coinor-libclp-dev
+g++
+libfl-dev
+libibex-dev
+libnlopt-cxx-dev
+libpython3-dev
+pkg-config
+python3-distutils
+python3-minimal
+zlib1g-dev
+EOF
+)
+
+if [[ ! -e /usr/bin/python ]]; then
+    apt-get install --no-install-recommends python-is-python3
+fi
+
+# Install bazel
+BAZEL_VERSION=3.0.0
+BAZEL_DEBNAME=bazel_${BAZEL_VERSION}-linux-x86_64.deb
+BAZEL_URL=https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${BAZEL_DEBNAME}
+BAZEL_SHA256=dfa79c10bbfa39cd778e1813a273fd3236beb495497baa046f26d393c58bdc35
+apt-get install -y --no-install-recommends wget
+wget "${BAZEL_URL}"
+if echo "${BAZEL_SHA256}  ${BAZEL_DEBNAME}" | sha256sum -c; then
+    apt-get install --no-install-recommends -y ./"${BAZEL_DEBNAME}"
+    rm "${BAZEL_DEBNAME}"
+else
+    echo "SHA256 does not match ${BAZEL_DEBNAME}:"
+    echo "    expected: ${BAZEL_SHA256}"
+    echo "    actual  : $(sha256sum "${BAZEL_DEBNAME}")"
+    exit 1
+fi
